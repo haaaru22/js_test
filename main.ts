43c5b340bb8710fafa7e65053a385eb8d49edd5a
haru <haru@pop-os.localdomain>
2021-04-07 21:12:04 +0900

    object

--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
-const array: number [] = [1,2,3];
+// number
+// string
+// boolean
+
+// function add(n1:number, n2:number){
+//     return n1 + n2;
+// }
+// const number1= 5;
+// const number2 = 1.8;
+// const result = add(number1, number2)
+// console.log(result);
 
 
-console.log(array);
+// const array: number [] = [1,2,3];
+// console.log(array);
+
+
+// let sum = (s1:number, s2:number, s3:boolean) => {
+//     return s1 + s2;
+// }
+// console.log(sum(4,3.6,true));
+
+//object
